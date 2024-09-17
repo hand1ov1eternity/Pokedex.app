19813e24fabec636e45421c80c25f2702cbc17b5
hand1ov1eternity <absurdism@duck.com>
2024-09-17 13:12:30 +0200

    reset and begin actual app

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 5,
+    types: ['water']
+  }
+];
