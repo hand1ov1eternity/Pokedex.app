23bbaa3f664d7a3f568f64b78753b3e8f449969f
hand1ov1eternity <absurdism@duck.com>
2024-09-18 13:58:47 +0200

    highlight pokemon`s height

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,17 @@ let pokemonList = [
   }
 ];
 
+// Loop through each Pokémon in the pokemonList array
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<p>'+ pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+  // Store the current Pokémon in a variable for easier access
+  let pokemon = pokemonList[i];
+  
+  // Write the Pokémon's name and height to the document
+  if (pokemon.height > 6) {
+    // If the Pokémon's height is greater than 6, add a special note
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that’s big!</p>');
+  } else {
+    // Otherwise, just display the Pokémon's name and height without any special note
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')</p>');
+  }
 }
