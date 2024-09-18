4d68f7e11c081f3720ce18a56ca96f13b25c5350
hand1ov1eternity <absurdism@duck.com>
2024-09-18 12:21:34 +0200

    add a loop

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,7 @@ let pokemonList = [
     types: ['water']
   }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<p>'+ pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+}
