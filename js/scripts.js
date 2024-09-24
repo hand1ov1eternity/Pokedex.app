725b1aa85c6ad6aef6cf54f800fdda0ac3f256a6
hand1ov1eternity <absurdism@duck.com>
2024-09-24 14:34:33 +0200

    Updated pokemonList iteration to use forEach method

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,10 @@ let pokemonList = [
   }
 ];
 
-// Loop through each Pokémon in the pokemonList array
-for (let i = 0; i < pokemonList.length; i++) {
-  // Store the current Pokémon in a variable for easier access
-  let pokemon = pokemonList[i];
-  
-  // Write the Pokémon's name and height to the document
+pokemonList.forEach(function(pokemon) {
   if (pokemon.height > 6) {
-    // If the Pokémon's height is greater than 6, add a special note
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that’s big!</p>');
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that’s big! </p>');
   } else {
-    // Otherwise, just display the Pokémon's name and height without any special note
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')</p>');
+    document.write('<p>' + pokemon.name + '(height: ' + pokemon.height + ')</p>');
   }
-}
+});
