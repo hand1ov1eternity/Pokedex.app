253f2344162d7f152b1566d6467742bbc0783056
hand1ov1eternity <absurdism@duck.com>
2024-09-24 15:19:11 +0200

    Encapsulated pokemonList in IIFE with getAll/add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-let pokemonList = [
+//IIFE to encapsulate pokemonList
+let pokemonRepository = (function(){
+ let pokemonList = [
   {
     name: 'Bulbasaur',
     height: 7,
@@ -16,10 +18,41 @@ let pokemonList = [
   }
 ];
 
-pokemonList.forEach(function(pokemon) {
+//Funktion to return all items in pokemonList
+function getAll() {
+  return pokemonList;
+}
+
+//Funktion to add a new item to pokemonList
+function add(pokemon) {
+  if (typeof pokemon === 'object' && pokemon.name && pokemon.height && pokemon.types) {
+    pokemonList.push(pokemon);
+  }else {
+    console.log('Invalid Pokemon entry. Please provide a valid object with name, height, and types.')
+  }
+}
+
+//Return an object exposing the getAll and add functions
+return {
+  getAll: getAll,
+  add: add
+};
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height > 6) {
     document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that’s big! </p>');
   } else {
     document.write('<p>' + pokemon.name + '(height: ' + pokemon.height + ')</p>');
   }
 });
+
+//Adding new Pokemon to the repository
+pokemonRepository.add({
+  name:'Pikachu',
+  height: 4,
+  types: ['electric']
+});
+
+//Verify the new Pokemon is added
+console.log(pokemonRepository.getAll());
