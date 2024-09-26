2cdc9993cc1873f7f9ad951f3907278bbc68b91e
hand1ov1eternity <absurdism@duck.com>
2024-09-26 15:17:22 +0200

    style index for all screen sizes, addListItem and showDetails functions added

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,76 @@
-//IIFE to encapsulate pokemonList
-let pokemonRepository = (function(){
- let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander',
-    height: 6,
-    types: ['fire']
-  },
-  {
-    name: 'Squirtle',
-    height: 5,
-    types: ['water']
+// IIFE to encapsulate pokemonList
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander',
+      height: 6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle',
+      height: 5,
+      types: ['water']
+    }
+  ];
+
+  // Function to return all items in pokemonList
+  function getAll() {
+    return pokemonList;
   }
-];
 
-//Funktion to return all items in pokemonList
-function getAll() {
-  return pokemonList;
-}
-
-//Funktion to add a new item to pokemonList
-function add(pokemon) {
-  if (typeof pokemon === 'object' && pokemon.name && pokemon.height && pokemon.types) {
-    pokemonList.push(pokemon);
-  }else {
-    console.log('Invalid Pokemon entry. Please provide a valid object with name, height, and types.')
+  // Function to add a new item to pokemonList
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && pokemon.name && pokemon.height && pokemon.types) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Invalid Pokemon entry. Please provide a valid object with name, height, and types.');
+    }
   }
-}
 
-//Return an object exposing the getAll and add functions
-return {
-  getAll: getAll,
-  add: add
-};
+  // Function to add a list item for a single Pokémon
+  function addListItem(pokemon) {
+    let listItem = document.createElement('li'); // Creating a new <li> element
+    let button = document.createElement('button'); // Creating a new button element
+    button.innerText = pokemon.name; // Setting the button's text to the Pokémon's name
+    button.classList.add('pokemon-button'); // Adding a class to the button
+
+       // Add event listener to the button
+       button.addEventListener('click', function() {
+        showDetails(pokemon); 
+      });
+    
+    // Appending the button to the list item
+    listItem.appendChild(button);
+    
+    // Appending the list item to the Pokémon list
+    let pokemonListElement = document.querySelector('.pokemon-list'); // Selecting the ul element
+    pokemonListElement.appendChild(listItem); // Appending the list item to the ul
+  }
+
+  // Return an object exposing the getAll, add, and addListItem functions
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem
+  };
 })();
 
-//Adding new Pokemon to the repository
+// Adding new Pokémon to the repository
 pokemonRepository.add({
-  name:'Pikachu',
+  name: 'Pikachu',
   height: 4,
   types: ['electric']
 });
 
-//Looping through pokemonList and displaying Pokemon
+// Looping through pokemonList and adding each item to the list
 pokemonRepository.getAll().forEach(function(pokemon) {
-  let listItem = document.createElement('li');
-
-  let button = document.createElement('button');
-  button.innerText = pokemon.name;
-
-
-button.classList.add('pokemon-button');
-
-listItem.appendChild(button);
-
-let pokemonListElement = document.querySelector('.pokemon-list');
-pokemonListElement.appendChild(listItem);
+  pokemonRepository.addListItem(pokemon); // Using the addListItem function
 });
 
-
-//Verify the new Pokemon is added
+// Verify the new Pokémon is added
 console.log(pokemonRepository.getAll());
